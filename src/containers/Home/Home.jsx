675e3e84ff676196b733e889dacd3bc4bfe9ccc3
AniModi <98632268+AniModi@users.noreply.github.com>
2023-12-09 01:39:26 +0530

    Added dialog box

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Home.scss";
 import GasCoupon from "../../components/GasCoupon/GasCoupon";
+import DialogBox from "../../components/DialogBox/DialogBox";
+import useToggle from "../../hooks/useToggle";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useToggle(false)
   return (
     <>
+    <DialogBox isOpen={isOpen} setIsOpen={setIsOpen}></DialogBox>
       <div className="home_container">
         <div className="home_container__header">
           <div className="home_container__header__title">
             <p>My Passes</p>
           </div>
           <div className="home_container__header__btn_container">
-            <button>Acquire a new pass</button>
+            <button onClick={setIsOpen}>Acquire a new pass</button>
           </div>
         </div>
         <div className="home_container__body">
